140854258dcdb63e3b5abe1a5620b1ada67f490c
Juan Nadar Yanac Barreto <juan.yanac@outlook.com>
2019-05-25 21:17:41 -0300

    Main view design

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -14,9 +15,12 @@ public class Main extends Application {
 		try {
 			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
+			ScrollPane scrollpane = loader.load();
 			//Scene scene = new Scene(root,400,400);
-			Scene scene = new Scene(parent);
+			Scene scene = new Scene(scrollpane);
+			
+			scrollpane.setFitToHeight(true);
+			scrollpane.setFitToWidth(true);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			primaryStage.setScene(scene);
